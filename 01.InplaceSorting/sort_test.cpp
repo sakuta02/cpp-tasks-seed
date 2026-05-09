5c090789d0c97579b11fa3c4bf24852a380e26a9
Saveliy <savam.0510@gmail.com>
2026-05-09 03:29:26 +0300

    Solve 01.InplaceSorting

--- 01.InplaceSorting/sort_test.cpp
+++ 01.InplaceSorting/sort_test.cpp
@@ -1,4 +1,6 @@
 #include <gtest/gtest.h>
+#include <algorithm>
+#include <vector>
 
 // А вот тут collvalue.h не надо, берите просто инты
 #include "sorting.h"
@@ -7,5 +9,42 @@
 
 TEST(FooSuite, FooTest)
 {
-    EXPECT_EQ(0, 0);
+    std::vector<int> a = {5, 4, 3, 2, 1};
+    bubble_sort(a.begin(), a.end());
+    EXPECT_TRUE(std::is_sorted(a.begin(), a.end()));
+}
+
+TEST(FooSuite, FooTest2)
+{
+    std::vector<int> a = {7};
+    bubble_sort(a.begin(), a.end());
+    EXPECT_TRUE(std::is_sorted(a.begin(), a.end()));
+}
+
+TEST(FooSuite, FooTest3)
+{
+    std::vector<int> a;
+    bubble_sort(a.begin(), a.end());
+    EXPECT_TRUE(std::is_sorted(a.begin(), a.end()));
+}
+
+TEST(FooSuite, FooTest4)
+{
+    std::vector<int> a = {9, 1, 8, 2, 7, 3, 6, 4, 5};
+    quick_sort(a.begin(), a.end());
+    EXPECT_TRUE(std::is_sorted(a.begin(), a.end()));
+}
+
+TEST(FooSuite, FooTest5)
+{
+    std::vector<int> a = {3, 3, 2, 2, 1, 1, 3, 2, 1};
+    quick_sort(a.begin(), a.end());
+    EXPECT_TRUE(std::is_sorted(a.begin(), a.end()));
+}
+
+TEST(FooSuite, FooTest6)
+{
+    std::vector<int> a = {1, 2, 3, 4, 5, 6};
+    quick_sort(a.begin(), a.end());
+    EXPECT_TRUE(std::is_sorted(a.begin(), a.end()));
 }
--- 01.InplaceSorting/sorting.h
+++ 01.InplaceSorting/sorting.h
@@ -3,6 +3,8 @@
 
 // Header-only
 
+#include <utility>
+
 // Для swap
 #include "collvalue.h"
 
@@ -10,12 +12,65 @@
 template <typename Iterator>
 void bubble_sort(Iterator begin, Iterator end)
 {
+    if (begin == end)
+    {
+        return;
+    }
+
+    for (Iterator i = begin; i != end; ++i)
+    {
+        for (Iterator j = begin; j + 1 != end; ++j)
+        {
+            if (*(j + 1) < *j)
+            {
+                using std::swap;
+                swap(*j, *(j + 1));
+            }
+        }
+    }
 }
 
 // --------------------------- QuickSort (например) ---------------------------------------
 template <typename Iterator>
 void quick_sort(Iterator begin, Iterator end)
 {
+    if (end - begin <= 1)
+    {
+        return;
+    }
+
+    Iterator left = begin;
+    Iterator right = end - 1;
+    auto mid = begin + (end - begin) / 2;
+    auto pivot = *mid;
+
+    while (left <= right)
+    {
+        while (*left < pivot)
+        {
+            ++left;
+        }
+        while (pivot < *right)
+        {
+            --right;
+        }
+        if (left <= right)
+        {
+            using std::swap;
+            swap(*left, *right);
+            ++left;
+            --right;
+        }
+    }
+
+    if (begin < right + 1)
+    {
+        quick_sort(begin, right + 1);
+    }
+    if (left < end)
+    {
+        quick_sort(left, end);
+    }
 }
 
 #endif // SORTING_H
